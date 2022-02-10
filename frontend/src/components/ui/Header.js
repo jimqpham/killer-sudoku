35d055c893ab3506251b822a543a9aa619e58ae0
Jim Pham <jimqpham@gmail.com>
2022-02-09 20:02:29 -0700

    Polish UI elements

--- frontend/src/components/ui/Header.js
+++ frontend/src/components/ui/Header.js
@@ -1,16 +1,49 @@
-import styles from "./Header.module.css";
-import { Link } from "react-router-dom";
+import { useEffect, useState } from "react";
+import { useDispatch } from "react-redux";
 
-const Header = () => {
+import styles from "./Header.module.css";
+
+import { Link } from "react-router-dom";
+import getUserRole from "../../helpers/getUserRole";
+import gameplayActions from "../../context/gameplay-slice";
+
+const Header = (props) => {
+	const [userRole, setUserRole] = useState(null);
+
+	const dispatch = useDispatch();
+
+	useEffect(() => {
+		const getRole = async () => {
+			const role = await getUserRole();
+			setUserRole(role);
+		};
+
+		getRole();
+	}, []);
+
+	const clearSession = () => {
+		dispatch(gameplayActions.clear());
+		localStorage.setItem("sdk-token", "");
+	};
+
+	const showInstruction = () => {
+		props.showInstruction();
+	};
+
 	return (
 		<div className={styles.header}>
 			<img className={styles.logo} src="logo.png" alt="Killer Sudoku" />
 			<nav className={styles.nav}>
-				<Link className={styles.navLink} to="/game">
-					Play Game
+				<Link className={styles.navLink} onClick={showInstruction} to="/game">
+					Instructions
 				</Link>
-				<Link className={styles.navLink} to="/buildyourown">
-					Build Your Own
+				{userRole === "admin" && (
+					<Link className={styles.navLink} to="/buildyourown">
+						Build Your Own
+					</Link>
+				)}
+				<Link className={styles.navLink} onClick={clearSession} to="/">
+					Logout
 				</Link>
 			</nav>
 		</div>
--- frontend/src/components/ui/Modal.js
+++ frontend/src/components/ui/Modal.js
@@ -1,9 +1,15 @@
 import styles from "./Modal.module.css";
 
 const Modal = (props) => {
+	const closeModal = () => {
+		if (props.closeModal) {
+			props.closeModal();
+		}
+	};
+
 	return (
 		<>
-			<div className={styles.backdrop} />
+			<div className={styles.backdrop} onClick={closeModal} />
 			<div className={styles.modal}>
 				<div className={styles.title}>{props.title}</div>
 				<div className={styles.content}>{props.content}</div>
